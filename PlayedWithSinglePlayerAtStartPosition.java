b6668d98535c6bbd7780da782f627a4f39a9b4d1
krutikabhadane77 <krutikabhadane588@gmail.com>
2022-10-08 13:34:00 +0530

    Added: UC-1 Snake & Ladder game played with single player at start position 0

---
+++ PlayedWithSinglePlayerAtStartPosition.java
@@ -0,0 +1,13 @@
+package Snake_and_Ladder_Problem;
+public class PlayedWithSinglePlayerAtStartPosition {
+    PlayedWithSinglePlayerAtStartPosition(){
+        System.out.println("Welcome to Snake and Ladder Problem");
+        }
+        public static void main(String args[]){
+            PlayedWithSinglePlayerAtStartPosition a = new PlayedWithSinglePlayerAtStartPosition();
+
+            //UC-1 Snake & Ladder game played with single played at start position.
+            int start_position=0;
+            System.out.println("Single player at starting position "+start_position);
+        }
+}
